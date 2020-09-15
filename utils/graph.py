38a4ccde59c49e135388daa4ec868c6578674d77
Kevin <cchong0124@gmail.com>
2020-09-15 10:47:24 +0800

    add saving

--- utils/graph.py
+++ utils/graph.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+import os
 
+def add_dir(name): 
+    path = "../figure/"+name
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
-
-def loss_graph(model_):
+def loss_graph(model_, name):
     plt.plot(model_.history['loss']) 
     plt.plot(model_.history['val_loss']) 
     plt.title('model loss')
@@ -10,8 +14,8 @@ def loss_graph(model_):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left') 
     plt.show()
-    # plt.savefig("figure/loss.png")
-def recall_graph(model_):
+    plt.savefig("../figure/loss.png")
+def recall_graph(model_, name):
     plt.plot(model_.history['recall']) 
     plt.plot(model_.history['val_recall']) 
     plt.title('model recall')
@@ -19,8 +23,8 @@ def recall_graph(model_):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left') 
     plt.show()
-    # plt.savefig("figure/recall.png")
-def precision_graph(model_):
+    plt.savefig("../figure/recall.png")
+def precision_graph(model_, name):
     plt.plot(model_.history['precision']) 
     plt.plot(model_.history['val_precision']) 
     plt.title('model precision')
@@ -28,12 +32,12 @@ def precision_graph(model_):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left') 
     plt.show()
-    # plt.savefig("figure/precision.png")
-def lr_graph(model_):
+    plt.savefig("../figure/precision.png")
+def lr_graph(model_, name):
     plt.plot(model_.history['lr']) 
     plt.title('model Learning Rate')
     plt.ylabel('lr')
     plt.xlabel('epoch')
     plt.legend(['lr'], loc='upper left') 
     plt.show()
-    # plt.savefig("figure/lr_decay.png")
+    plt.savefig("../figure/"+name+"lr_decay.png")
