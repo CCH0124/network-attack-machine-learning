c1afa1bf26638fb9fd0207cd9cd825b87f75059c
Kevin <cchong0124@gmail.com>
2020-09-21 01:08:43 +0800

    add decay lr function

--- utils/learningrate_decay.py
+++ utils/learningrate_decay.py
@@ -1,6 +1,15 @@
+
 import numpy as np
-def exp_decay(self, epoch):
-    initial_lrate = self.__learning_rate
+def step_decay(epoch):
+	initial_lrate = 0.1
+	drop = 0.5
+	epochs_drop = 10.0
+	lrate = initial_lrate * np.power(drop, np.floor((1+epoch)/epochs_drop))
+	return lrate
+
+
+def exp_decay(epoch, lr):
+    initial_lrate = lr
     k = 0.15
     lrate = initial_lrate * np.exp(-k*epoch)
     return lrate
