4c3f49fb6814dfe79f67eb0de884e18b7f89e556
Kevin <cchong0124@gmail.com>
2020-09-28 13:36:26 +0800

    add predict preprocess

--- utils/data_preprocess.py
+++ utils/data_preprocess.py
@@ -71,4 +71,12 @@ class preprocess():
         # print(X_train)
         # return self.splite_train_test()
         return X_train, X_test, y_train, y_test
-        
+    def predict_preprocessing(self):
+        self.get_dataFrame()
+        # self.__dataFrame = ndp.inf_and_na_drop(self.__dataFrame)
+        # self.__dataFrame = ndp.inf_drop_insert_nan(self.__dataFrame)
+        # self.__dataFrame = ndp.inf_replace_value_ffill_nan(self.__dataFrame)
+        self.__dataFrame = ndp.inf_drop_insert_nan(self.__dataFrame, method='quadratic')
+        self.one_hot_encode()
+        self.normalization()
+        return self.__dataFrame, self.__LabelDataFrame
