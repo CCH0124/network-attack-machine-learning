ad711320d07e4baca8665add19d90648b8ba53d6
Kevin <cchong0124@gmail.com>
2020-10-02 14:04:25 +0800

    add preprocessing function

--- utils/data_preprocess.py
+++ utils/data_preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import utils.data_feature as feature
-import utils.data_miss_value as ndp
+import utils.data_value as ndp
 
 class preprocess():
     __filePath = None
@@ -12,6 +12,8 @@ class preprocess():
     __test_size = None
     __depth = None
     __feature_selection = None
+    __del_column = None
+
     def __init__(self, filePath, feature_selection=None, labelColumnName='Label', depth=3, test_size=0.3):
         super().__init__()
         self.__filePath = filePath
@@ -22,15 +24,13 @@ class preprocess():
     
     def get_dataFrame(self):
         self.__dataFrame = pd.read_csv(self.__filePath)
-        self.column_trim()
+        ndp.column_trim(self.__dataFrame)
         if self.__feature_selection == None:
             self.__dataFrame = self.__dataFrame[feature.get_feature()]
         else:
             self.__dataFrame = self.__dataFrame[self.__feature_selection]
-
-    def column_trim(self):
-        self.__dataFrame.columns = self.__dataFrame.columns.str.strip()
-
+        self.__del_column = ndp.column_del_one_value(self.__dataFrame)
+    
     def __get_Label(self):
         label_list = self.__getAttackList()
         label = self.__dataFrame.pop(self.__labelColumnName)
@@ -61,6 +61,7 @@ class preprocess():
         
     def preprocessing(self):
         self.get_dataFrame()
+        self.__dataFrame.drop(self.__del_column, axis=1, inplace=True)
         # self.__dataFrame = ndp.inf_and_na_drop(self.__dataFrame)
         # self.__dataFrame = ndp.inf_drop_insert_nan(self.__dataFrame)
         # self.__dataFrame = ndp.inf_replace_value_ffill_nan(self.__dataFrame)
@@ -71,8 +72,10 @@ class preprocess():
         # print(X_train)
         # return self.splite_train_test()
         return X_train, X_test, y_train, y_test
+
     def predict_preprocessing(self):
         self.get_dataFrame()
+        self.__dataFrame.drop(self.__del_column, axis=1, inplace=True)
         # self.__dataFrame = ndp.inf_and_na_drop(self.__dataFrame)
         # self.__dataFrame = ndp.inf_drop_insert_nan(self.__dataFrame)
         # self.__dataFrame = ndp.inf_replace_value_ffill_nan(self.__dataFrame)
